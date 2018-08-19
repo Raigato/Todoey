7b11cce040759290a5a0c665d9835d99463b3c4c
Gabriel Raymondou <gabrielraymondou@macbook-pro-de-gabriel.local>
2018-08-19 10:42:02 +0200

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Gabriel Raymondou on 19/08/2018.
+//  Copyright © 2018 Gabriel Raymondou. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
